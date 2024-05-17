5f8680d84d4486c3b7189ee4511d69bc8a9b74ef
KuanKuan <leaker233@icloud.com>
2024-05-17 10:32:43 +0800

    Save DBGWCR & DBGWVR during a mode switch

--- arch/arm64/kernel/asm-offsets.c
+++ arch/arm64/kernel/asm-offsets.c
@@ -73,6 +73,8 @@ int main(void)
   DEFINE(S_ORIG_ADDR_LIMIT,	offsetof(struct pt_regs, orig_addr_limit));
   DEFINE(S_PMR_SAVE,		offsetof(struct pt_regs, pmr_save));
   DEFINE(S_STACKFRAME,		offsetof(struct pt_regs, stackframe));
+  DEFINE(S_xx,		offsetof(struct pt_regs, xx));
+  DEFINE(S_yy,		offsetof(struct pt_regs, yy));
   DEFINE(S_FRAME_SIZE,		sizeof(struct pt_regs));
   BLANK();
 #ifdef CONFIG_COMPAT
